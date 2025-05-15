101af41d3038b5bdec3729c97f0980921dc6155e
Jesse Higginson <kadmium@gmail.com>
2025-05-15 23:21:54 +0000

    Fix delta time and add tests

--- src/packet/midi_packet/delta_time.rs
+++ src/packet/midi_packet/delta_time.rs
@@ -10,13 +10,20 @@ pub trait DeltaTimeChunkExt {
     fn time(&self) -> u8;
 }
 
+const CONTINUATION_MASK: u8 = 0b1000_0000;
+const TIME_MASK: u8 = 0b0111_1111;
+const U28_MAX: u32 = 0b0000_1111_1111_1111_1111_1111_1111_1111;
+const U21_MAX: u32 = 0b0000_0000_0001_1111_1111_1111_1111_1111;
+const U14_MAX: u32 = 0b0000_0000_0000_0000_0011_1111_1111_1111;
+const U7_MAX: u32 = 0b0000_0000_0000_0000_0000_0000_0111_1111;
+
 impl DeltaTimeChunkExt for u8 {
     fn continuation(&self) -> bool {
-        self & 0b1000_0000 != 0
+        self & CONTINUATION_MASK != 0
     }
 
     fn time(&self) -> u8 {
-        self & 0b0111_1111
+        self & TIME_MASK
     }
 }
 
@@ -26,30 +33,42 @@ impl DeltaTime {
     }
 
     pub fn new(delta_time: u32) -> Self {
-        let mut chunks = Vec::new();
-        let mut remaining_time = delta_time;
-
-        while remaining_time > 0 {
-            let time = (remaining_time & 0x7F) as u8;
-            // Compose the chunk byte: set the high bit if more chunks follow
-            let continuation = if remaining_time > 0x7F {
-                0b1000_0000
-            } else {
-                0
-            };
-            chunks.push(continuation | time);
-            remaining_time >>= 7;
+        if delta_time > U28_MAX {
+            panic!("Delta time exceeds maximum value of {}", U28_MAX);
         }
-
-        DeltaTime { chunks }
+        if delta_time > U21_MAX {
+            let chunks = vec![
+                ((delta_time >> 21) & U7_MAX) as u8,
+                ((delta_time >> 14) & U7_MAX) as u8 | CONTINUATION_MASK,
+                ((delta_time >> 7) & U7_MAX) as u8 | CONTINUATION_MASK,
+                (delta_time & U7_MAX) as u8 | CONTINUATION_MASK,
+            ];
+            return DeltaTime { chunks };
+        } else if delta_time > U14_MAX {
+            let chunks = vec![
+                ((delta_time >> 14) & U7_MAX) as u8,
+                ((delta_time >> 7) & U7_MAX) as u8 | CONTINUATION_MASK,
+                (delta_time & U7_MAX) as u8 | CONTINUATION_MASK,
+            ];
+            return DeltaTime { chunks };
+        } else if delta_time > U7_MAX {
+            let chunks = vec![
+                ((delta_time >> 7) & U7_MAX) as u8,
+                (delta_time & U7_MAX) as u8 | CONTINUATION_MASK,
+            ];
+            return DeltaTime { chunks };
+        } else {
+            let chunks = vec![(delta_time & U7_MAX) as u8];
+            return DeltaTime { chunks };
+        }
     }
 
     pub fn time(&self) -> u32 {
         let mut time = 0;
         for (i, chunk) in self.chunks.iter().enumerate() {
-            time |= (*chunk as u32 & 0x7F) << (7 * i);
+            time |= ((*chunk & TIME_MASK) as u32) << (7 * (self.chunks.len() - i - 1));
         }
-        time
+        return time;
     }
 
     pub fn size(&self) -> usize {
@@ -79,5 +98,54 @@ impl DeltaTime {
 impl std::fmt::Debug for DeltaTime {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "DeltaTime {{ {:?} }}", self.time())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_delta_time_one_byte() {
+        let delta_time = DeltaTime::new(0b0000_1111);
+        assert_eq!(delta_time.time(), 0b0000_1111);
+        assert_eq!(delta_time.size(), 1);
+
+        let mut bytes = [0; 1];
+        delta_time.write_to_bytes(&mut bytes).unwrap();
+        assert_eq!(bytes, [0b0000_1111]);
+    }
+
+    #[test]
+    fn test_delta_time_two_bytes() {
+        let delta_time = DeltaTime::new(0b1111_1111);
+        assert_eq!(delta_time.time(), 0b1111_1111);
+        assert_eq!(delta_time.size(), 2);
+
+        let mut bytes = [0; 2];
+        delta_time.write_to_bytes(&mut bytes).unwrap();
+        assert_eq!(bytes, [0b0000_0001, 0b1111_1111]);
+    }
+
+    #[test]
+    fn test_delta_time_three_bytes() {
+        let delta_time = DeltaTime::new(0b1111_1111_1111_1111);
+        assert_eq!(delta_time.time(), 0b1111_1111_1111_1111);
+        assert_eq!(delta_time.size(), 3);
+
+        let mut bytes = [0; 3];
+        delta_time.write_to_bytes(&mut bytes).unwrap();
+        assert_eq!(bytes, [0b0000_0011, 0b1111_1111, 0b1111_1111]);
+    }
+
+    #[test]
+    fn test_delta_time_four_bytes() {
+        let delta_time = DeltaTime::new(0b0011_1111_1111_1111_1111_1111);
+        assert_eq!(delta_time.time(), 0b0011_1111_1111_1111_1111_1111);
+        assert_eq!(delta_time.size(), 4);
+
+        let mut bytes = [0; 4];
+        delta_time.write_to_bytes(&mut bytes).unwrap();
+        assert_eq!(bytes, [0b0000_0001, 0b1111_1111, 0b1111_1111, 0b1111_1111]);
     }
 }
